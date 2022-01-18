5f0a23cc898e2690d9e231594e526a2c01f3ce82
bcjaeger <byron.jaeger@gmail.com>
2022-01-18 16:21:36 -0500

    splitting bp fig; easier to read

--- R/bp_long_viz.R
+++ R/bp_long_viz.R
@@ -8,13 +8,15 @@
 bp_long_viz <- function(date = "01-15-22") {
 
   cols_bg <- viridis(3)
-  cols_tx <- c("#DF8F44FF","#ED0000FF","#79AF97FF","#00468BFF")
+  cols_tx <- pal_lancet()(2)
   cols_diff <- c("maroon4","black")
 
   .date <- str_remove_all(date, fixed('-'))
 
   fname <- paste0("SPRINT_EHR_SBP_GLIMMIX_LongTerm_", .date, ".csv")
   fname2 <- paste0("SPRINT_EHR_SBP_GLIMMIX_JAMA_IM_REDO_", .date, ".csv")
+
+  # Blood pressure levels over time ----
 
   bp <- read_csv(file.path('ehr', fname)) |>
     select(-Statement, -Alpha, -tValue, -DF, -StdErr) |>
@@ -33,8 +35,8 @@ bp_long_viz <- function(date = "01-15-22") {
   bp_levels <- filter(bp, group %in% c("Standard (EHR)",
                                        "Intensive (EHR)") & time_months>54)
 
-  bp_levels_trial <- filter(bp_trial, group %in% c("STD",
-                                                   "INT") & source=="EHR") |>
+  bp_levels_trial <- bp_trial |>
+    filter(group %in% c("STD", "INT") & source=="EHR") |>
     select(-source) |>
     mutate(group = recode(group,
                           "STD" = "Standard (EHR)",
@@ -50,52 +52,67 @@ bp_long_viz <- function(date = "01-15-22") {
                           "STD" = "Standard (trial)",
                           "INT" = "Intensive (trial)",
                           "DIFF" = "Difference (trial)"))
-  bp_levels<-rbind(bp_levels_trial, bp_levels)
+
+  bp_levels<-rbind(bp_levels_trial, bp_levels) |>
+    separate(group,
+             into = c('treatment', 'source'),
+             sep = ' ') |>
+    mutate(source = str_remove_all(source, pattern = '\\(|\\)'),
+           source = recode(source, 'trial' = 'Trial'),
+           treatment = factor(treatment, levels = c("Standard", "Intensive")))
+
+  # Blood pressure difference over time ----
 
   bp_diff_orig <- filter(bp, group == 'Difference (EHR)' & time_months>54)
-  bp_diff_ehr<- filter(bp_trial, group=="DIFF" & source=="EHR") |> select(-source) |>
+
+  bp_diff_ehr<- bp_trial |>
+    filter(group=="DIFF" & source=="EHR") |>
+    select(-source) |>
     mutate(group = recode(group, "DIFF" = "Difference (EHR)"))
-  bp_diff_clinic<- filter(bp_trial, group=="DIFF" & source=="CLINIC") |> select(-source) |>
+
+  bp_diff_clinic <- bp_trial |>
+    filter(group=="DIFF" & source=="CLINIC") |>
+    select(-source) |>
     mutate(group = recode(group, "DIFF" = "Difference (trial)"))
 
-  bp_diff<-rbind(bp_diff_ehr,bp_diff_orig,bp_diff_clinic)
+  bp_diff <- rbind(bp_diff_ehr,
+                   bp_diff_orig,
+                   bp_diff_clinic) |>
+    separate(group,
+             into = c('drop_me', 'source'),
+             sep = ' ') |>
+    mutate(source = str_remove_all(source, pattern = '\\(|\\)'),
+           source = recode(source, 'trial' = 'Trial'),
+           source = fct_reorder2(source, .x = time_months, .y = Estimate)) |>
+    select(-drop_me)
 
-  bp_targets <- tibble(
-    x = c(0, 0),
-    xend = c(2.54, 2.54),
-    y = c(150, 110),
-    yend = c(150, 110),
-    group = c("Standard", "Intensive")
-  )
-
-  bp_diff_targets <- tibble(
-    x = 0,
-    xend = 2.54,
-    y = 20,
-    yend = 20
-  )
+  # Figure; BP levels ----
 
   fig_levels <- ggplot(bp_levels) +
     aes(x = time_months/12,
         y = Estimate,
-        color = group) +
-    geom_line(size = 1) +
+        color = treatment,
+        linetype = source) +
+    geom_line(size = 0.85) +
     labs(y = 'Systolic blood pressure, mm Hg',
          x = 'Years since randomization',
          fill = 'Treatment',
-         color = 'Treatment') +
+         color = 'Treatment',
+         linetype = 'Data Source') +
     scale_x_continuous(limits = c(0,10),
                        breaks = seq(0,10)) +
     scale_y_continuous(limits = c(110, 152),
                        breaks = seq(110, 150, by=5)) +
     scale_color_manual(values = cols_tx) +
     scale_fill_manual(values = cols_tx) +
-    theme_fig(legend.position = 'top') +
+    scale_linetype_manual(values = c(1,2)) +
+    theme_fig(legend.position = 'right') +
     geom_ribbon(alpha = 0.2,
                 mapping = aes(x = time_months/12,
                               ymin = Lower,
                               ymax = Upper,
-                              fill = group),
+                              fill = treatment,
+                              group = interaction(treatment, source)),
                 inherit.aes = FALSE)
 
   fig_levels <-
@@ -104,27 +121,28 @@ bp_long_viz <- function(date = "01-15-22") {
                     ymax = 152,
                     xmax = 10,
                     ymin = 110,
-                    ymult = 0.95,
+                    ymult = 0.965,
                     x_cohort_phase = 6)
+
+  # Figure; BP Diff ----
 
   fig_diff <- ggplot(bp_diff) +
     aes(x = time_months/12,
         y = Estimate,
-        color=group) +
-    geom_line(size = 1) +
-    geom_ribbon(aes(ymin = Lower, ymax = Upper, fill=group),
+        linetype = source) +
+    geom_line(color = cols_tx[2], size = 0.85) +
+    geom_ribbon(aes(ymin = Lower, ymax = Upper),
+                fill = cols_tx[2],
                 alpha = 0.2) +
-    scale_color_manual(values = cols_diff) +
-    scale_fill_manual(values = cols_diff) +
     labs(y = 'Between-group difference in systolic blood pressure, mm Hg',
          x = 'Years since randomization',
-         fill = 'Treatment',
-         color = 'Treatment') +
+         linetype = 'Data Source') +
+    scale_linetype_manual(values = c(2,1)) +
     scale_x_continuous(limits = c(0,10),
                        breaks = seq(0,10)) +
     scale_y_continuous(limits = c(-5, 25),
                        breaks = seq(-5,25, by=5)) +
-    theme_fig(legend.position = 'top')
+    theme_fig(legend.position = 'right')
 
   fig_diff <-
     add_annotations(fig_diff,
